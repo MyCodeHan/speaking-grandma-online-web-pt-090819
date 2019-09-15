d37533c6defa20d78488a3052294eec695bf7924
Hannah Harrington <hannahharrington@outlook.com>
2019-09-15 19:50:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,7 @@ def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!" 
     # If you shout, she can hear you (or at least she thinks so)
+<<<<<<< HEAD
 elsif string == string.upcase
   return "NO, NOT SINCE 1938!"
   # and yells back
@@ -15,4 +16,23 @@ else
     # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
  end
+=======
+    # and yells back
+  elsif string == "Hi"
+  		return "HUH?! SPEAK UP, SONNY!"
+  elsif string == string.downcase
+      return "HUH?! SPEAK UP, SONNY!"
+  
+    # NO, NOT SINCE 1938!
+  elsif string == "DID YOU EAT TODAY?"
+      return "NO, NOT SINCE 1938!"
+    else
+  		return "NO, NOT SINCE 1938!"
+    end
+  elsif string == "Hi"
+      return  "HUH?! SPEAK UP, SONNY!"
+# However if you say 'I LOVE YOU GRANDMA!', she should respond with
+# 'I LOVE YOU TOO PUMPKIN!'
+
+>>>>>>> a3ef4e20ab6db0f0008f6a1e6338cd83cc94ef7f
 end
